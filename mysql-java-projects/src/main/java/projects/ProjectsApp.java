b6e07fef567f148268a80da8c00b70418b49e7c7
MKerris <116782244+MKerris@users.noreply.github.com>
2022-12-31 12:33:17 -0700

    week7 work

---
+++ mysql-java-projects/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,13 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+
+		DbConnection.getConnection();
+		
+	}
+
+}
---
+++ mysql-java-projects/src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,34 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+	private static final String SCHEMA = "projects";
+	private static final String USER = "projects";
+	private static final String PASSWORD = "projects";
+	private static final String HOST = "localhost";
+	private static final int PORT = 3306;
+
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
+	
+		System.out.println("Connecting with url=" + url);
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Successfully obtained connection");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("Error getting connection");
+			throw new DbException(e);
+		}
+
+		
+	
+	}
+
+}
